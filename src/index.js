7e673854dd7be2237cf5faeac12c82860eb96603
Prince <chimeremezechukwudire@gmail.com>
2020-07-22 23:50:11 +0100

    script  modified

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import home from './components/home'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const home = () => {
+    console.log('i am learning this react cby force')
+    return ( <
+        div >
+        <
+        p > Thi is home < /p> < /
+        div >
+    )
+}
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    console.log(now)
+    return ( <
+        div >
+        <
+        p > Hello world, it is { now.toString() } < /p> <
+        p > { a }
+        plus { b }
+        is { a + b } <
+        /p>   < /
+        div >
+        <
+        home / >
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+    )
+}
+ReactDOM.render( < App / > , document.getElementById('root'))
