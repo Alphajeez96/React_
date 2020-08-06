897dad6d64bd1768786cdf5d41db04fb3776807b
Prince <chimeremezechukwudire@gmail.com>
2020-08-07 00:42:52 +0100

    counter

--- src/index.js
+++ src/index.js
@@ -1,47 +1,20 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-const Hello = (props) => {
-    const bornYear = () => {
-        const yearNow = new Date().getFullYear();
-        return yearNow - props.age;
-    };
-
-    return ( <
-        div >
-        <
-        p >
-        Hello { props.name }, you are { props.age }
-        years old { " " } <
-        /p>{" "} <
-        p > So you were probably born in { bornYear() } < /p>{" "} < /
-        div >
-    );
-};
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-    const name = "Peter";
-    const age = prompt("how old are you");
+    const [counter, setCounter] = useState(0)
 
-
-
-
-
-
-
-
-
-
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
 
     return ( <
-        div >
-        <
-        h1 > Greetings < /h1> <Hello name="Maya" age={26 + 10} / > { " " } <
-        Hello name = { name }
-        age = { age }
-        />{" "} < /
-        div >
-    );
-};
+        div > { counter } < /div>
+    )
+}
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render( <
+    App / > ,
+    document.getElementById('root')
+)
